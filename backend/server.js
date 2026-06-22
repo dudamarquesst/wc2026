c5fb10e6649cb1de274db21dbfefc8257c414198
dudamarquesst <duda.marques@sempreceub.com>
2026-06-22 23:29:13 +0000

    fix: adiciona GitHub Pages no CORS

--- backend/server.js
+++ backend/server.js
@@ -88,6 +88,7 @@ app.use(
       "http://localhost:5173",
       "http://127.0.0.1:5173",
       /\.app\.github\.dev$/,
+      "https://dudamarquesst.github.io",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
